54e11266b9b1e5a2cccd1954a4e25b41bb4887ac
Abhay Kasturia <abhayk@ccs.neu.edu>
2016-07-02 09:00:03 -0400

    openshift changes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
 
-var connectionString = 'mongodb://localhost/cs5610summer1';
+// var connectionString = 'mongodb://localhost/cs5610summer1';
 
-// var connectionString = 'mongodb://127.0.0.1:27017/cs5610summer1';
-//
-// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-//         process.env.OPENSHIFT_APP_NAME;
-// }
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610summer1';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
 
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
